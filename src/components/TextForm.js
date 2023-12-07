2665154d24ef1bcd2cee9cb7dcbdc89151a9fe9b
ravi2977 <mauryaravi599@gmail.com>
2023-12-07 23:02:47 +0530

    To capitalise and clear text buttons added to the page to capitalise the text and clear the text box

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,36 +1,45 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 
 
 export default function TextForm(props) {
-    const[text, setText] = useState('');
-    const handleUpClick=()=>{
+    const [text, setText] = useState('');
+    const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleLowerClick=()=>{
+    const handleLowerClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
     }
-    const handleRemoveExtraSpaces=()=>{
+    const handleRemoveExtraSpaces = () => {
         let newText = text.split(" ");
-        let s="";
-        for(let i = 0;i<newText.length;i++){
-           if(newText[i] ===''){
-               continue
-           }
-           else{
-            s+=newText[i]+" ";
-           }
+        let s = "";
+        for (let i = 0; i < newText.length; i++) {
+            if (newText[i] === '') {
+                continue
+            }
+            else {
+                s += newText[i] + " ";
+            }
+        }
+        setText(s);
+    }
+    const handleCapitalise = () => {
+        let newText = text.split(" ");
+        let s = "";
+        for (let i = 0; i < newText.length; i++) {
+            if(newText[i] !== "")
+                s=s+" "+newText[i].charAt(0).toUpperCase()+newText[i].slice(1);
         }
         setText(s);
     }
 
-    const handleOnChange=(event)=>{
+    const handleOnChange = (event) => {
         console.log("HandleOn Chnaged");
         setText(event.target.value);
     }
-    const handleClearText=()=>{
+    const handleClearText = () => {
         setText("");
     }
     return (
@@ -41,19 +50,19 @@ export default function TextForm(props) {
                 <div className="my-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} rows="8" id="myBox"></textarea>
                 </div>
-               <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert To UpperCase</button>
-               <button className="btn btn-primary mx-2 my-2" onClick={handleLowerClick}>Convert To LowerCase</button>
-               <button className="btn btn-primary mx-2 my-2" onClick={handleRemoveExtraSpaces}>Reamove Extra Spaces</button>
-               <button className="btn btn-primary mx-2 my-2" onClick={handleClearText}>Clear Text</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert To UpperCase</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={handleLowerClick}>Convert To LowerCase</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={handleRemoveExtraSpaces}>Reamove Extra Spaces</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={handleCapitalise}>Capitalise the Text</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={handleClearText}>Clear Text</button>
             </div>
             <div className="container my-3">
                 <h2>Your Text Summery</h2>
-                <p>{text.split(" ").length -1} words and {text.length} charecters</p>
-                <p>{(0.008*text.split(" ").length)-0.008} minutes to read </p>
+                <p>{text.split(" ").length - 1} words and {text.length} charecters</p>
+                <p>{(0.008 * text.split(" ").length) - 0.008} minutes to read </p>
                 <h2>Privew</h2>
                 <p>{text}</p>
             </div>
         </div>
     )
 }
- 
