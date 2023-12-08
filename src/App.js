253c9c60feb8442d63717bf008ff8f8e9ab501dd
ravi2977 <mauryaravi599@gmail.com>
2023-12-08 12:34:52 +0530

    Updated dark mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import './App.css';
+import AboutUs from './components/AboutUs';
+import React, { useState } from 'react'
+
+
 function App() {
+  const [mode, setMode] = useState('light')//check that dark mode enable or not
+  const toggleMode = () => {
+    if (mode === 'dark') {
+      setMode('light');
+      document.body.style.backgroundColor = "white"
+    }
+    else {
+      setMode('dark')
+      document.body.style.backgroundColor = "rgb(39, 41, 40)"
+
+    }
+  }
   return (
     <>
-      <Navbar title = "Text-Changer" about = "About Text-Changer"/>
-      <TextForm heading = "Enter your Text to Analyse"/>
+      <Navbar title="Text-Changer" about="About Text-Changer" mode={mode} toggleMode={toggleMode} />
+      <TextForm heading="Enter your Text to Analyse" mode={mode} />
+      <AboutUs mode={mode}/>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,7 +30,7 @@ export default function TextForm(props) {
         let s = "";
         for (let i = 0; i < newText.length; i++) {
             if(newText[i] !== "")
-                s=s+" "+newText[i].charAt(0).toUpperCase()+newText[i].slice(1);
+                s=s+" "+newText[i].charAt(0).toUpperCase()+newText[i].slice(1).toLowerCase();
         }
         setText(s);
     }
@@ -42,22 +42,28 @@ export default function TextForm(props) {
     const handleClearText = () => {
         setText("");
     }
+    const handleCopy=()=>{
+        var text = document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
     return (
         <div>
 
             <div className="container my-3">
-                <h1>{props.heading}</h1>
+                <h1 style={{color: props.mode ==='dark'?'white':'rgb(39, 41, 40)'}}>{props.heading}</h1>
                 <div className="my-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} rows="8" id="myBox"></textarea>
+                    <textarea className="form-control" style={{backgroundColor: props.mode ==='dark'?'rgb(39, 41, 40)':'white',color: props.mode ==='dark'?'white':'rgb(39, 41, 40)'}} value={text} onChange={handleOnChange} rows="8" id="myBox"></textarea>
                 </div>
                 <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert To UpperCase</button>
                 <button className="btn btn-primary mx-2 my-2" onClick={handleLowerClick}>Convert To LowerCase</button>
                 <button className="btn btn-primary mx-2 my-2" onClick={handleRemoveExtraSpaces}>Reamove Extra Spaces</button>
                 <button className="btn btn-primary mx-2 my-2" onClick={handleCapitalise}>Capitalise the Text</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={handleCopy}>CopybText</button>
                 <button className="btn btn-primary mx-2 my-2" onClick={handleClearText}>Clear Text</button>
             </div>
-            <div className="container my-3">
-                <h2>Your Text Summery</h2>
+            <div className="container my-3" style={{color: props.mode ==='dark'?'white':'rgb(39, 41, 40)'}}>
+                <h2 >Your Text Summery</h2>
                 <p>{text.split(" ").length - 1} words and {text.length} charecters</p>
                 <p>{(0.008 * text.split(" ").length) - 0.008} minutes to read </p>
                 <h2>Privew</h2>
